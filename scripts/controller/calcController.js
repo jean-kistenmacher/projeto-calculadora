8bd5e2a29ef945c63b9458d51aa4487ea2b9613d
jean-kistenmacher <jkist.coder@gmail.com>
2020-05-19 22:26:31 -0300

    Add Calculator Initialize

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -1,1 +1,33 @@
-class CalcController {}
+class CalcController {
+  constructor() {
+    this._displayCalc = "0";
+    this._currentDate;
+    this.initialize();
+  }
+
+  initialize() {
+    let displayCalcEl = document.querySelector("#display");
+    let dateEl = document.querySelector("#data");
+    let timeEl = document.querySelector("#hora");
+
+    displayCalcEl.innerHTML = "123456789";
+    dateEl.innerHTML = "01/05/2020";
+    timeEl.innerHTML = "17:30";
+  }
+
+  get displayCalc() {
+    return this._displayCalc;
+  }
+
+  set displayCalc(value) {
+    this._displayCalc = value;
+  }
+
+  get currentDate() {
+    return this._currentDate;
+  }
+
+  set currentDate(value) {
+    this._currentDate = value;
+  }
+}

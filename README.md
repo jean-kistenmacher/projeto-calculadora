e8ddf0a6ff7891dfa4aa5d360e34d0acc2dd88a7
jean-kistenmacher <jkist.coder@gmail.com>
2020-05-20 21:34:18 -0300

    add display date/time and buttons event listener

--- README.md
+++ README.md
@@ -2,7 +2,8 @@
 
 Calculadora desenvolvida para o Curso Completo de JavaScript da HCode.
 
-[![Hcode Treinamentos]](https://www.hcode.com.br)
+[Hcode Treinamentos](https://www.hcode.com.br)
 
 ### Projeto
+
 ![Calculadora](https://firebasestorage.googleapis.com/v0/b/hcode-com-br.appspot.com/o/calculadora-hcode.jpg?alt=media&token=5406aa3f-b965-401c-9b4e-654609c78b33)
--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -1,30 +1,75 @@
 class CalcController {
   constructor() {
-    this._displayCalc = "0";
+    this._locale = "pt-BR";
+    this._displayCalcEl = document.querySelector("#display");
+    this._dateEl = document.querySelector("#data");
+    this._timeEl = document.querySelector("#hora");
     this._currentDate;
     this.initialize();
+    this.initButtonsEvents();
   }
 
   initialize() {
-    let displayCalcEl = document.querySelector("#display");
-    let dateEl = document.querySelector("#data");
-    let timeEl = document.querySelector("#hora");
+    this.setDisplayDateTime();
+    setInterval(() => {
+      this.setDisplayDateTime();
+    }, 1000);
+  }
 
-    displayCalcEl.innerHTML = "123456789";
-    dateEl.innerHTML = "01/05/2020";
-    timeEl.innerHTML = "17:30";
+  addEventListenerAll(element, events, fn) {
+    events.split(" ").forEach((event) => {
+      element.addEventListener(event, fn, false);
+    });
+  }
+
+  initButtonsEvents() {
+    let buttons = document.querySelectorAll("#buttons > g, #parts > g");
+    buttons.forEach((btn, index) => {
+      this.addEventListenerAll(btn, "click drag", (e) => {
+        console.log(btn.className.baseVal.replace("btn-", ""));
+      });
+
+      this.addEventListenerAll(btn, "mouseover mouseup mousedown", (e) => {
+        btn.style.cursor = "pointer";
+      });
+    });
+  }
+
+  setDisplayDateTime() {
+    this.displayDate = this.currentDate.toLocaleDateString(this._locale, {
+      day: "2-digit",
+      month: "long",
+      year: "numeric",
+    });
+    this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
+  }
+
+  set displayTime(value) {
+    this._timeEl.innerHTML = value;
+  }
+
+  get displayTime() {
+    return this._timeEl.innerHTML;
+  }
+
+  set displayDate(value) {
+    this._dateEl.innerHTML = value;
+  }
+
+  get displayDate() {
+    return this._dateEl.innerHTML;
   }
 
   get displayCalc() {
-    return this._displayCalc;
+    return this._displayCalcEl.innerHTML;
   }
 
   set displayCalc(value) {
-    this._displayCalc = value;
+    this._displayCalcEl.innerHTML = value;
   }
 
   get currentDate() {
-    return this._currentDate;
+    return new Date();
   }
 
   set currentDate(value) {

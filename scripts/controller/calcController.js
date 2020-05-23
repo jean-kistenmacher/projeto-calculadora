367e9ab13160337a9c0f771572289f36f8177c69
jean-kistenmacher <jkist.coder@gmail.com>
2020-05-23 02:52:50 -0300

    add copy and paste

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -11,6 +11,23 @@ class CalcController {
     this.initialize();
     this.initButtonsEvents();
     this.initKeyboard();
+    this.pasteFromClipboard();
+  }
+
+  copyToClipboard() {
+    let input = document.createElement("input");
+    input.value = this.displayCalc;
+    document.body.appendChild(input);
+    input.select();
+    document.execCommand("Copy");
+    input.remove();
+  }
+
+  pasteFromClipboard() {
+    document.addEventListener("paste", (e) => {
+      let text = e.clipboardData.getData("Text");
+      this.displayCalc = parseFloat(text);
+    });
   }
 
   initialize() {
@@ -57,6 +74,9 @@ class CalcController {
         case "8":
         case "9":
           this.addOperation(parseInt(e.key));
+          break;
+        case "c":
+          if (e.ctrlKey) this.copyToClipboard();
           break;
       }
     });
